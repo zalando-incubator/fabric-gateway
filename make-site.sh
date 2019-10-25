964150249adcd2845e5f5f37a5033ab6e8440d82
Francis Moloney <francis.moloney@zalando.ie>
2019-10-25 11:16:53 +0100

    replace image, fix botched replace
    Signed-off-by: Francis Moloney <francis.moloney@zalando.ie>

--- make-site.sh
+++ make-site.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+current_version=master-4
+
 cd docs-raw/site
 mkdocs build -d ../../docs
 
 cd ../..
 combined_install='docs/assets/basic-install.yaml'
-cat deploy/operator/apply/01_FabricGatewayCRD.yaml > ${combined_install}
-echo -e '\n---\n' >> ${combined_install}
-cat deploy/operator/apply/02_GatewayCRDSchemaValidation.yaml >> ${combined_install}
-echo -e '\n---\n' >> ${combined_install}
-cat deploy/operator/apply/03_MetacontrollerGatewaySynchHook.yaml >> ${combined_install}
-echo -e '\n---\n' >> ${combined_install}
-cat deploy/operator/apply/07_OperatorDeployment.yaml >> ${combined_install}
-echo -e '\n---\n' >> ${combined_install}
-cat deploy/operator/apply/08_OperatorService.yaml >> ${combined_install}
+cat deploy/operator/apply/01_FabricGatewayCRD.yaml > ""${combined_install}""
+echo -e '\n---\n' >> "${combined_install}"
+cat deploy/operator/apply/02_GatewayCRDSchemaValidation.yaml >> "${combined_install}"
+echo -e '\n---\n' >> "${combined_install}"
+cat deploy/operator/apply/03_MetacontrollerGatewaySynchHook.yaml >> "${combined_install}"
+echo -e '\n---\n' >> "${combined_install}"
+cat deploy/operator/apply/08_OperatorService.yaml >> "${combined_install}"
+echo -e '\n---\n' >> "${combined_install}"
+cat deploy/operator/apply/07_OperatorDeployment.yaml >> "${combined_install}"
+sed -i '' 's#{{{IMAGE}}}#registry.opensource.zalan.do/fabric/gateway-k8s-operator#' "${combined_install}"
+sed -i '' "s/{{{VERSION}}}/${current_version}/" "${combined_install}"
