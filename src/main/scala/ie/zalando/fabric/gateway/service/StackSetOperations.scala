d1387c4e66f94a34a234464bd990e762f9bb4dea
Conor Gallagher <conor.gallagher@zalando.ie>
2020-03-25 14:26:06 +0000

    MID-2558 Adding support for the use of named service ports  (#31)
    * MID-2558 Adding support for the use of named service ports in addition to numeric ports in StackSets status subresource.
    
    Signed-off-by: Conor Gallagher <conor.gallagher@zalando.ie>
    
    * Converting to implicit instantiation to match below
    
    Signed-off-by: Conor Gallagher <conor.gallagher@zalando.ie>
    
    * Removing unused implicit conversions
    
    Signed-off-by: Conor Gallagher <conor.gallagher@zalando.ie>

--- src/main/scala/ie/zalando/fabric/gateway/service/StackSetOperations.scala
+++ src/main/scala/ie/zalando/fabric/gateway/service/StackSetOperations.scala
@@ -1,5 +1,6 @@
 package ie.zalando.fabric.gateway.service
 
+import ie.zalando.fabric.gateway.models.SynchDomain.{K8sServicePortIdentifier, NamedServicePort, NumericServicePort}
 import org.slf4j.{Logger, LoggerFactory}
 import play.api.libs.json._
 import skuber._
@@ -8,10 +9,10 @@ import skuber.api.client.KubernetesClient
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-case class StackSetIdentifer(name: String, namespace: String)
+case class StackSetIdentifier(name: String, namespace: String)
 case class StackSetIngress(backendPort: Int)
 case class StackSetSpec(externalIngress: Option[StackSetIngress])
-case class StackDefinedService(serviceName: String, servicePort: Int, weight: Int)
+case class StackDefinedService(serviceName: String, servicePort: K8sServicePortIdentifier, weight: Int)
 case class StackSetStatus(readyStacks: Int, stacks: Int, stacksWithTraffic: Int, traffic: Option[Seq[StackDefinedService]])
 
 class StackSetOperations(k8sClient: KubernetesClient)(implicit execCtxt: ExecutionContext) {
@@ -19,6 +20,16 @@ class StackSetOperations(k8sClient: KubernetesClient)(implicit execCtxt: Executi
   private val log: Logger = LoggerFactory.getLogger(classOf[StackSetOperations])
 
   private type StackSet = CustomResource[StackSetSpec, StackSetStatus]
+
+  implicit val servicePortReads: Reads[K8sServicePortIdentifier] = {
+    case JsString(s) => JsSuccess(NamedServicePort(s))
+    case other => Reads.IntReads.reads(other).map(NumericServicePort)
+  }
+  
+  implicit val servicePortWrites: Writes[K8sServicePortIdentifier] = {
+    case NamedServicePort(name) => JsString(name)
+    case NumericServicePort(port) => JsNumber(port)
+  }
 
   implicit val stackSvcFmt: Format[StackDefinedService] = Json.format[StackDefinedService]
   implicit val stackIngressFmt: Format[StackSetIngress] = Json.format[StackSetIngress]
@@ -31,7 +42,7 @@ class StackSetOperations(k8sClient: KubernetesClient)(implicit execCtxt: Executi
     version = "v1"
   )
 
-  def getStatus(stackSet: StackSetIdentifer): Future[Option[StackSetStatus]] = {
+  def getStatus(stackSet: StackSetIdentifier): Future[Option[StackSetStatus]] = {
     k8sClient
       .getInNamespace[StackSet](stackSet.name, stackSet.namespace)
       .map(_.status)
