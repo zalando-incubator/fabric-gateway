8c03361f6f27a902179a5226ce8ca913e9d22c72
bmooney <brian.mooney@zalando.ie>
2020-04-02 16:33:41 +0100

    MID-2573: Service Auditing not happening for whitelist rejection routes
      * Update test to check for correct scenario
    
    Signed-off-by: bmooney <brian.mooney@zalando.ie>

--- src/test/scala/ie/zalando/fabric/gateway/service/IngressDerivationChainSpec.scala
+++ src/test/scala/ie/zalando/fabric/gateway/service/IngressDerivationChainSpec.scala
@@ -465,11 +465,13 @@ class IngressDerivationChainSpec extends FlatSpec with MockitoSugar with Matcher
 
   it should "have audit logging enabled for the 403 routes" in {
     val routes = testableWhitelistRoutesDerivation(sampleResourceWhitelistingGateway)
-      .filter(isCatchAllRoute)
+      .filter { ingressDef =>
+        ingressDef.metadata.name.contains("non-whitelisted")
+      }
       .collect {
         case IngressDefinition(_, IngressMetaData(SkipperRouteDefinition(_, _, _, Some(customRoute), _), _, _)) => customRoute
       }
-    routes.size shouldBe 1
+    routes.size shouldBe 2
     routes.forall { route =>
       route.filters.toList.contains(AccessLogAuditing(AccessLogAuditing.ServiceRealmTokenIdentifierKey))
     } shouldBe true
