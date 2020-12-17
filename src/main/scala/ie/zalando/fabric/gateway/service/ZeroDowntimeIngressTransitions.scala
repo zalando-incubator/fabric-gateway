ffcd256754aa398215923df0821b8e29d72cd8c9
bmooney <brian.mooney@zalando.ie>
2020-12-17 09:58:12 +0000

    MID-2411: Bump apiVersion for ingress
      * Another test
    
    Signed-off-by: bmooney <brian.mooney@zalando.ie>

--- src/main/scala/ie/zalando/fabric/gateway/service/ZeroDowntimeIngressTransitions.scala
+++ src/main/scala/ie/zalando/fabric/gateway/service/ZeroDowntimeIngressTransitions.scala
@@ -1,20 +1,19 @@
 package ie.zalando.fabric.gateway.service
 
-import ie.zalando.fabric.gateway.models.HttpModels
 import ie.zalando.fabric.gateway.models.SynchDomain.{GatewayMeta, GatewaySpec, IngressDefinition}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class ZeroDowntimeIngressTransitions(ingressDerivationChain: IngressDerivationChain) {
 
-  def defineSafeRouteTransition(spec: GatewaySpec, metadata: GatewayMeta, existingRoutes: Seq[IngressDefinition], isLegacy: Boolean)(
+  def defineSafeRouteTransition(spec: GatewaySpec, metadata: GatewayMeta, existingRoutes: Seq[IngressDefinition])(
       implicit ec: ExecutionContext): Future[List[IngressDefinition]] = {
     ingressDerivationChain
       .deriveRoutesFor(spec, metadata)
       .map { desiredRoutes =>
         val deleted = diff(existingRoutes, desiredRoutes)
         val created = diff(desiredRoutes, existingRoutes)
-        (if (created.isEmpty) desiredRoutes else desiredRoutes ++ deleted).map(r => applyMigration(r, isLegacy))
+        (if (created.isEmpty) desiredRoutes else desiredRoutes ++ deleted).map(applyMigration)
       }
   }
 
@@ -23,9 +22,8 @@ class ZeroDowntimeIngressTransitions(ingressDerivationChain: IngressDerivationCh
       b.map(_.metadata.name).contains(i.metadata.name)
     }
 
-  private def applyMigration(ingress: IngressDefinition, isLegacy: Boolean): IngressDefinition = {
-    if (isLegacy) ingress.copy(apiVersion = HttpModels.LegacyIngressApiVersion)
-    else if (!ingress.metadata.name.startsWith("m-")) ingress.copy(metadata = ingress.metadata.copy(name = s"m-${ingress.metadata.name}"))
+  private def applyMigration(ingress: IngressDefinition): IngressDefinition = {
+    if (!ingress.metadata.name.startsWith("m-")) ingress.copy(metadata = ingress.metadata.copy(name = s"m-${ingress.metadata.name}"))
     else ingress
   }
 }
